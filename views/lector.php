b08782b810d100a9ef8a34079431989612d4698f
Jesus <jdgonzalez815@misena.edu.co>
2022-10-15 13:13:38 -0500

    libreria lector

--- views/lector.php
+++ views/lector.php
@@ -21,6 +21,8 @@ if( $validar == null || $validar = ''){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/634962e990.js" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" href="../css/fontawesome-all.min.css">
 
 <link rel="stylesheet" href="../css/estilo.css">
